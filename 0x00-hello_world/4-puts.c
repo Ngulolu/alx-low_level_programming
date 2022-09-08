f9c235f3a5a131863758c40eb5997304227909b9
Ngulolu <b.ngulolu@gmail.com>
2022-09-08 06:13:36 -0700

    Is a C program that prints using put fxn

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/* Description : main - Prints out a string to stdout .
+   Return : 0 if sucess.
+*/
+int main(void)
+{
+  puts ("\programming is like building a multingual puzzle");
+  return(0);
+
+}
