d042a9a7377ee9b20c5022ab61f735a0fa04f775
Ngulolu <b.ngulolu@gmail.com>
2022-09-13 00:34:57 -0700

    prints the string _putchar using character arrays

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "_putchar.h"
+
+/**
+ *main - prints the string '_putchar' using a character array.
+ *Return 0: when succesful 
+ */
+int main(void)
+{
+char c [ ] = "_putchar"
+int a;
+for (a=0, a<9, a++)
+  {
+    _putchar (l[a]);
+  }
+_putchar('\n');
+return (0);
+}
