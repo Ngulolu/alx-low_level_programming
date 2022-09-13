429a76a3321728e11244004e5df03081f1c16efb
Ngulolu <b.ngulolu@gmail.com>
2022-09-13 07:39:25 -0700

    Prints the string _putchar using character array

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,13 @@
-#include "_putchar.c"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-  char *sh = "putchar.c";
-  while (*sh)
-    {
-      _putchar(*sh);
-      sh++;
-    }
-  _putchar('\n');
+#include <unistd.h>
 
-  return (0);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+  return (write(1, &c, 1));
 }
