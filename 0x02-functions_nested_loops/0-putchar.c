3d3160756148f0d1b4f096c19b8e37917fb40570
Ngulolu <b.ngulolu@gmail.com>
2022-09-14 01:30:03 -0700

    Prints a string using char arrays

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,17 +2,18 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * main - prints the string "Holberton" from a character array.
- * Return: 0 on success.
+ *main - entry point.
+ *Return: 0 on sucses.
  */
 int main(void)
 {
-  char c[] = "_putchar";
-  int i;
-  for (i = 0; i < 9; i++)
-    {
-      _putchar(c[i]);
-    }
-  _putchar('\n');
-  return (0);
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
