87ec2dd3fd3cfd1430466414c07e160f8c421cfb
Ngulolu <b.ngulolu@gmail.com>
2022-09-14 05:01:40 -0700

    uses _is lower to check for lowercase char

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *_islower - checks for lowercase char
+ *
+ * Return : 1 if input(int i)is lowercase, 0, if otherwise
+ */
+
+int _islower(int i)
+{
+	if (i >= 'a' && i <= 'z')
+		return (1);
+	else
+		return (0);
+}
