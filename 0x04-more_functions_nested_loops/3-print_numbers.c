cae1393e8c360d443b24de2a30195d0c2c13862f
Ngulolu <b.ngulolu@gmail.com>
2022-09-16 01:51:09 -0700

    prints numbers from 0 to 9 followed by a new line

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_numbers - prints 0123456789\n
  *
- * Return: Always 0.
+ * Return: void
  */
-int main(void)
+
+
+void print_numbers(void)
 {
-    print_numbers();
-    return (0);
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
