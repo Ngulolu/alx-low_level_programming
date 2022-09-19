046097cfde666c2cdc3cdcce110699d1ba027ea0
Ngulolu <b.ngulolu@gmail.com>
2022-09-19 00:52:08 -0700

    prints a string followed by a new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,14 +6,15 @@
  *
  *Return : the length of a string
  */
+
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i] != "\0")
+	while (*(str + i))
 	{
-		_putchar(str[i]);
+		_putchar(*(str + i));
 		i++;
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
