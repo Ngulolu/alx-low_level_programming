6b65ae6139131cb59bd4c7dbf95a7e1981d8ec2c
Ngulolu <b.ngulolu@gmail.com>
2022-09-19 11:26:24 -0700

    reverses the characters of a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,19 +1,31 @@
+#include "main.h"
 /**
- *rev_string - reverses a string
- *@s:holi
+ * rev_string - reverses a string
+ * @s: holi
  *
  */
-
 void rev_string(char *s)
 {
-int i;
+	int i;
+	int j;
+	int temp;
+	int length;
 
-	i = 0;
-	while (s[i] != '\0')
+	while (s[i] != 0)
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-			s[i] = s[i] - 32;
 		i++;
 	}
-	return (s);
+
+	length = i;
+	i = 0;
+	j = length - 1;
+
+	while (i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
+	}
 }
