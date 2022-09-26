548c8ed2ea7ad42e952a2ead70baa713ad7831fe
Ngulolu <b.ngulolu@gmail.com>
2022-09-26 03:25:57 -0700

    prints the sum of the two diagonals of a square matrix of integers. Allowed to use the standard library

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *
  * set_string - sets the value of a pointer to a char
  * @s: double pointer to set
  * @to: the char to point to.
+ *Return : void
  */
 
 void set_string(char **s, char *to)
