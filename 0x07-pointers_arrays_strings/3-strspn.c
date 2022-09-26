46a4b6a995bcbbc8e0fba759c995607e14984653
Ngulolu <b.ngulolu@gmail.com>
2022-09-26 03:56:15 -0700

    gets the length of a prefix in a substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,23 +3,23 @@
 /**
  *_strspn - gets the length of a prefix substring
  *@s : string to check
- *@fate: string to check against
+ *@accept: string to check against
  *
  *Return: number of bytes of s in fate
  */
 
-unsigned int _strspn(char *s, char *fate)
+unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; fate[j]; j++)
+	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; fate[j]; j++)
+		for (j = 0; accept[j]; j++)
 		{
-			if (s[i] == fate[j])
+			if (s[i] == accept[j])
 				break;
 		}
-		if (!fate[j])
+		if (!accept[j])
 			break;
 	}
 	return (i);
