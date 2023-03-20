cbbe0284a5e911cc6ccfbe2caa29e98b63d2d018
Ngulolu <b.ngulolu@gmail.com>
2023-03-20 06:49:11 -0700

    This is a function that frees the 2D array created by 3-alloc_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,23 +1,22 @@
 #include "main.h"
-#include  <stdlib.h>
-#include  <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- *free_grid - entry point
- *@grid - rows of the matrix
- *@height - columns of the matrix
- *Return: Nothing
+ * free_grid - Entry point
+ *@grid: rows of matrix
+ *@height: columns of string
+ * Return: a pointer to a 2 dimensional array of integers or null
  */
-
 void free_grid(int **grid, int height)
 {
 int i;
 int *p;
- 
+
 for (i = 0; i < height; i++)
 {
 p = grid[i];
-free (p);   
+free(p);
 }
 free(grid);
 }
