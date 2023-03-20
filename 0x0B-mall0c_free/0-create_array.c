162301a425e82a6667ce33b8241ed2c5e37ef910
Ngulolu <b.ngulolu@gmail.com>
2023-03-20 02:57:08 -0700

    This function creates an array of chars, and initializes it with a specific char

--- 0x0B-mall0c_free/0-create_array.c
+++ 0x0B-mall0c_free/0-create_array.c
@@ -3,29 +3,28 @@
 #include <stdlib.h>
 
 /**
- *create_array - Entry point 
- *@size: size of the array
- *@t : character of type char
- * 
- *Return ; a pointer to the array, or NULL if it fails
- */
+ *create_array - Entry point
+*@size: size of the array
+*@t : character of type char
+*
+*Return: a pointer to the array, or NULL if it fails
+*/
 
-char *create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char t)
 {
-  char *array = NULL;  /**Declaration of a pointer array of type char**/
-  unsigned int i;      /*Declaration of integer*/
+char *array = NULL;  /**Declaration of a pointer array of type char**/
+unsigned int i;      /*Declaration of integer*/
 
-  if (size == 0)
-    return (NULL);
-  if (size != 0)
-    {
-
-      array = (char *)malloc(size *sizeof(char));
-      if (array != NULL)
-	{
-	  for (i=0; i<size; i++)
-	    array[i] = c;
-	}
-    }
-  return (array);
+if (size == 0)
+return (NULL);
+if (size != 0)
+{
+array = (char *)malloc(size * sizeof(char));
+if (array != NULL)
+{
+for (i = 0; i < size; i++)
+array[i] = c;
+}
+}
+return (array);
 }
