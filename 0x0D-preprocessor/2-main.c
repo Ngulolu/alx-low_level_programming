ed50b5c461640c6e80637908373dc9bd32bfefc7
Ngulolu <b.ngulolu@gmail.com>
2023-03-22 00:08:17 -0700

    A file that outputs the name of input using _FILE_ macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function with no arguments
+ *
+ * Description: prints the name of the file used as input
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
