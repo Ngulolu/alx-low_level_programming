3a50be3bd22a065e0c2dc50a8e4a0cfcda2ee5e1
Ngulolu <b.ngulolu@gmail.com>
2023-03-23 06:22:36 -0700

    A function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,35 +1,67 @@
-#include "dog.h"
 #include <stdlib.h>
-
+#include "dog.h"
 /**
- * new_dog - entry point
- * @name: string from main, name of pet
- * @age: number from main, age of pet
- * @owner: string from main, owner of pet
- * Return: p
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: The string to copy
+ * Return: a pointer to the duplicated string, NULL if insufficient memory
+ * or if @str is NULL
+ */
+char *_strdup(char *str)
+{
+	char *ar;
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+	ar = malloc(sizeof(char) * (i + 1));
+	if (ar == NULL)
+		return (NULL);
+	while (str[j])
+	{
+		ar[j] = str[j];
+		j++;
+	}
+	ar[j] = 0;
+	return (ar);
+}
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: NULL if function fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
-	/* reserving memory to struct*/
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
+	dog_t *new;
+	char *ncpy;
+	char *ocpy;
+
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
 		return (NULL);
-	/* Cpunting name pointer*/
-	if (name == NULL)
+
+	ncpy = _strdup(name);
+	if (!ncpy && name)
 	{
-		free(p);
-		free(owner);
+		free(new);
 		return (NULL);
 	}
-	if (owner == NULL)
+	ocpy = _strdup(owner);
+	if (!ocpy && owner)
 	{
-		free(p);
-		free(name);
+		free(ncpy);
+		free(new);
 		return (NULL);
 	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
+
+	new->name = ncpy;
+	new->age = age;
+	new->owner = ocpy;
+
+	return (new);
 }
