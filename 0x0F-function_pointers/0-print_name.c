b592922ac26a456847d031d95f6f4d88cb87474b
Ngulolu <b.ngulolu@gmail.com>
2023-03-22 18:49:31 -0700

    A function 'f' that takes a name as an argument and returns it

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
  * print_name - print a name using the function supplied by the caller
@@ -10,5 +9,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+if (f == NULL)
+return;
 f(name);
 }
