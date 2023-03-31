4b00fe8f06bd0177c91b1fd9a97c505e3944c33f
Ngulolu <b.ngulolu@gmail.com>
2023-03-31 01:37:47 -0700

    Betty Fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,12 +15,12 @@ size_t print_list(const list_t *h)
 {
 size_t count = 0;
 
- for (; h != NULL; h = h->next,count++)
+for (; h != NULL; h = h->next, count++)
 {
-  if (h -> str == NULL)
-    printf("[0](nil)\n");
-  else
-    printf("[%d]%s\n", h ->len, h->str);
- }
+if (h->str == NULL)
+printf("[0](nil)\n");
+else
+printf("[%d]%s\n", h->len, h->str);
+}
 return (count);
 }
