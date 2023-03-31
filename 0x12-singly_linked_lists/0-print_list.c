cf6e27d8b3fbf3fa071dae0175845d85ac92199e
Ngulolu <b.ngulolu@gmail.com>
2023-03-31 01:32:17 -0700

    Prints all elements of struct list_t

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,12 +13,14 @@
 
 size_t print_list(const list_t *h)
 {
-int count = 0;
+size_t count = 0;
 
-for (; h != NULL; h = h->next)
+ for (; h != NULL; h = h->next,count++)
 {
-printf("%d\n", h->len);
-count++;
-}
+  if (h -> str == NULL)
+    printf("[0](nil)\n");
+  else
+    printf("[%d]%s\n", h ->len, h->str);
+ }
 return (count);
 }
