3a6405ae5cb79d83632bac0cbb9000be74a817c8
Ngulolu <b.ngulolu@gmail.com>
2023-04-03 01:33:44 -0700

    prints the number of nodes in a singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,30 +1,27 @@
-#include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
+#include "lists.h"
 
 /**
- *print_list(const list_t *h) - a function with one argument
- *@h: pointer to struct list_t
+ *print_list - function that takes one argument
+ *@h: const pointer type
  *
- *Description: prints all elements of a list
+ *Description: prints all elements of a list_t list
  *Return: number of nodes
  */
 
+
 size_t print_list(const list_t *h)
 {
-size_t count = 0;
+	unsigned int count = 0;
 
-for (; h != NULL; h = h->next)
-{
-if ((h->len) == 0)
-printf("[0]");
-else
-printf("[%d]", h->len);
-if (h->str == NULL)
-printf("[0](nil)\n");
-else
-printf("[%d]%s\n", h->len, h->str);
-}
-return (count);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
