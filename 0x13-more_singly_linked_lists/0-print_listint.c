ef6858817464d25bf24d21fbdf418377aef9a335
Ngulolu <b.ngulolu@gmail.com>
2023-04-05 01:53:01 -0700

    Edits

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *print_listint - print list
+ *Return: size_t
+ *@h: list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+ int nodes = 0;
+ const listint_t *current = h;
+  
+ for (; current != NULL; current = current->next)
+    {
+      printf("%d\n", current->n);
+      nodes++;
+    }
+  return (nodes);
+}
