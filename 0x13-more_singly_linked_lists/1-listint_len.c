25018082edb5da3ef56bf89452c51a7fd895dfd9
Ngulolu <b.ngulolu@gmail.com>
2023-04-05 02:51:08 -0700

    Description added for file 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ *listint_len - get len of list
+ *Return: size_t
+ *@h: list
+ */
+
 size_t listint_len(const listint_t *h)
 {
   unsigned int l = 0;
