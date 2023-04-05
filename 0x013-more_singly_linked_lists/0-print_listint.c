d172363ea5a13dc908b6bfb17e244a6e2eff3789
Ngulolu <b.ngulolu@gmail.com>
2023-04-05 01:51:09 -0700

    Edits

--- 0x013-more_singly_linked_lists/0-print_listint.c
+++ 0x013-more_singly_linked_lists/0-print_listint.c
@@ -9,10 +9,10 @@
 
 size_t print_listint(const listint_t *h)
 {
-  int nodes = 0;
-  const listint_t *current = h;
+ int nodes = 0;
+ const listint_t *current = h;
   
-  for(; current != NULL; current = current->next)
+ for (; current != NULL; current = current->next)
     {
       printf("%d\n", current->n);
       nodes++;
