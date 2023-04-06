eb2e26d46039ded5ed1e89cc5cc7fcfecd1eaffe
Ngulolu <b.ngulolu@gmail.com>
2023-04-06 15:44:14 -0700

    returns the number of bits you would need to flip to get from one no to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *flip_bits - a function that returns the number of bits to flip to get from one number to another
+ *@n: the first bit word
+ *@m: the second bit word
+ *
+ *Return: the hamming distance
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+  unsigned long int flipped = n ^ m;
+  unsigned int len = 0;
+  unsigned int i = 0;
+
+  /* iterate over each bit in flipped */
+
+  for (; i < sizeof(unsigned long int) * 8; i++)
+    {
+      if (flipped & (1UL << i))
+	{
+	  len++;
+	}
+    }
+  return len;
+}
