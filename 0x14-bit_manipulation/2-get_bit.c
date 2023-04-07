d6c34e4382a63cebda8b766034ccd8d8c7358753
Ngulolu <b.ngulolu@gmail.com>
2023-04-07 09:08:07 -0700

    returns the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,37 +1,21 @@
-#include <stdio.h>
-#include "main.h"
-
-/**
- *get_bit - a function that returns the value of a bit at a given index
- *@n: integer to get bit value from
- *@index: index of n to get bit from
- *
- *Return: bit at the particular index
- */
-
 int get_bit(unsigned long int n, unsigned int index)
 {
+  /* Check if index is greater than the bit size of n */
 
-  unsigned long int musk = 1UL << index;
+  unsigned long int mask = 1UL;
   unsigned int i = 0;
 
-  /*first check if index is greater than the bit size of an unsigned long int(8) */
-
-  if (index >= sizeof(unsigned long int) *8)
+  if (index >= sizeof(unsigned long int) * 8)
     {
-      return (-1);
+      return -1;
     }
 
-  /* Loops over bits of n from least signifcant bit to the index_th bit using the musk to test each */
+  /* Shift mask left until it has a single bit set at the given index */
+  while (i < index) {
+    mask <<= 1;
+    i++;
+  }
 
-  for (; i <= index; musk <<= 1)
-    {
-      if (i == index)
-	{
-	  return (n & musk) != 0;
-	}
-    }
-  /* incase it finishes loop without indexth bit, may never, return -1 */
-
-  return (-1);
+  /* Use the mask to check the bit value at the given index */
+  return (n & mask) != 0;
 }
