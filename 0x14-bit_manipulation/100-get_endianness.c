f9d527cb1e8ce18660924a610990f21c7e1fa477
Ngulolu <b.ngulolu@gmail.com>
2023-07-06 05:01:42 -0700

    Determine endianness of a system

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -14,26 +14,4 @@ unsigned int num = 1; /* Declare variable "num" type int with a value of 1 */
 char *byte = (char *)&num;  /* Create a pointer and set the address of num */
 
 return ((int)*byte); /*Return the value of "byte"pointed to by byte */
-
-}
-
-/**
- *main - entry point of the program
- *
- *Return: Always 0
- */
-
-int main(void)
-{
-int n;
-n = get_endianness();
-if (n != 0)
-{
-printf("least endian\n");
-}
-else
-{
-printf("large endian\n");
-}
-return (0);
 }
