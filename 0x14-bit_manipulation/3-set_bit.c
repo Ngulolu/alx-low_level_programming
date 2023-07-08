bf0dcda6141bf311219e1aa20a58002b801a9d9c
Ngulolu <b.ngulolu@gmail.com>
2023-07-08 01:25:05 -0700

    The magic of bitwise

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,17 +2,20 @@
 
 /**
  *set_bit - a function that sets the value of a bit
- *@n: the value a bit to be set to 1 at a given index
- *@index: the index to be set value for
+ *@n: the pointer to the number to which you want to set bit
+ *@index: the posiition of the bit which you want to set, starting from 0.
  *
- *Return: 1
+ *Return: -1
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int mask = 1UL << index; /*A mask initialized with 1 */
+if (index >= sizeof(unsigned long int)  * 8 || n == NULL)
+{
+return -1;
+}
 *n |= mask; /* use bitwise OR to set the bit at the index to 1 */
 
 return (1);
-
 }
