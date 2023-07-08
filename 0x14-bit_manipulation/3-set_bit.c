1288c82dd5ef36206871393d6aac17d5b7c7ca20
Ngulolu <b.ngulolu@gmail.com>
2023-07-08 01:28:33 -0700

    .

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,10 +10,12 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+
 unsigned long int mask = 1UL << index; /*A mask initialized with 1 */
 if (index >= sizeof(unsigned long int)  * 8 || n == NULL)
+
 {
-return -1;
+return (-1);
 }
 *n |= mask; /* use bitwise OR to set the bit at the index to 1 */
 
