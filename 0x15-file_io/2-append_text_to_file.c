3e67e3a860d225024ef1fd1206280b49c83dbd71
Ngulolu <b.ngulolu@gmail.com>
2023-07-11 00:56:50 -0700

    Betty Edits

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,7 +5,16 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
-int append_text_to_file(const char *filename, char * text_content)
+/**
+ *append_text_to_file - a function that appends text to end of a file
+ *@filename: the name to be appended text to
+ *@text_content: the text content to appended to a file
+ *
+ *Return: 0
+ */
+
+
+int append_text_to_file(const char *filename, char *text_content)
 {
 int fd;
 ssize_t bytes_written;
@@ -15,17 +24,17 @@ return (-1);
 
 fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
-return (-1);  
+return (-1);
 
-if(text_content != NULL)
+if (text_content != NULL)
 {
 bytes_written = write(fd, text_content, strlen(text_content));
-if(bytes_written == -1)
+if (bytes_written == -1)
 {
 close(fd);
 return (-1);
 }
 }
-close (fd);
-return (-1); 
+close(fd);
+return (-1);
 }
