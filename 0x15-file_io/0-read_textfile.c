c6a49d9b31f70acd16b9ef3b085b855ebd763105
Ngulolu <b.ngulolu@gmail.com>
2023-07-11 00:04:45 -0700

    reads a text file and prints out to POSIX

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,50 +1,51 @@
 #include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
  *read_textfile - a function that reads a text and prints it to POSIX stdout
  *@filename:pointer to the buffer
  *@letters: size of char to be printed
  *
+ *Return: 0
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+char *buffer = NULL;
+ssize_t l_read;
+ssize_t l_written;
+int fd;
 
-if (filename == NULL)
+if (!(filename && letters))
+return (0);
 
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+
+buffer = malloc(sizeof(char) * letters);
+if (!buffer)
+return (0);
+
+l_read = read(fd, buffer, letters);
+close(fd);
+
+if (l_read < 0)
 {
+free(buffer);
 return (0);
 }
 
-FILE *file = fopen(filename, "r");
-if (file == NULL)
+if (!l_read)
+l_read = letters;
 
-{
+l_written = write(STDOUT_FILENO, buffer, l_read);
+free(buffer);
+
+if (l_written < 0)
 return (0);
+
+return (l_written);
 }
-
-char *av = (char *) malloc(sizeof(char) * (letters + 1)); /*Allocates memory */
-if (av == NULL)
-{
-fclose(file);
-return (0);
-}
-
-ssize_t bytesRead;
-bytesRead = free(av, sizeof(char), ac, file);
-
-if (bytesRead < 0)
-{
-fclose(file);
-free(av);
-return (0); /* if read fails */
-}
-
-av[bytesRead] = '\0'; /* Add a null terminator to make it a string */
-
-printf("%s", av);
-
-fclose(file);
-free(av);
-
-return (bytesRead); /* Actual number of letters read and printed */
